7028976075b70dd1e6ec59662b39a8013c36b0e8
keerthitp_git <tp.keerthana.k@gmail.com>
2019-07-25 13:59:44 -0500

    Pushing the repository

---
+++ WineStoreProject/src/main/java/com/company/WineStoreProject/WineStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.WineStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineStoreProjectApplication.class, args);
+	}
+
+}
